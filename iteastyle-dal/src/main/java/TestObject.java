a9b9ce42657c91b794c517a594f4193b8d836239
aimeow <a18814888787@gmail.com>
2018-06-03 15:54:51 +0800

    update this code

--- iteastyle-dal/src/main/java/TestObject.java
+++ iteastyle-dal/src/main/java/TestObject.java
@@ -1,5 +1,5 @@
 public class TestObject {
     public String hello () {
-        return "hello world test test test TestObject";
+        return "hello world test test test TestObject twice";
     }
 }
