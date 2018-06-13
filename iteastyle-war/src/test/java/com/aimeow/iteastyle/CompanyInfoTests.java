e2aa29d97926f30b25c0e41c9ddfc327d24061d3
zhaoyi.w <a18814888787@gmail.com>
2018-06-13 19:20:31 +0800

    update these code

--- iteastyle-war/src/test/java/com/aimeow/iteastyle/CompanyInfoTests.java
+++ iteastyle-war/src/test/java/com/aimeow/iteastyle/CompanyInfoTests.java
@@ -1,6 +1,7 @@
 package com.aimeow.iteastyle;
 
 import com.aimeow.iteastyle.dao.CompanyInfoDAO;
+import com.aimeow.iteastyle.domain.CompanyInfoDO;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,5 +21,20 @@ public class CompanyInfoTests {
 
     @Test public void testGetCompanyInfo() throws Exception {
         System.out.println(companyInfoDAO.getCompanyInfo());
+    }
+
+    @Test public void testReplaceCompanyInfo() throws Exception {
+        CompanyInfoDO companyInfoDO = new CompanyInfoDO();
+
+        companyInfoDO.setCompanyInfo("companyInfo");
+        companyInfoDO.setPostCode("postCode");
+        companyInfoDO.setPhoneNumber("phoneNumber");
+        companyInfoDO.setTelephoneNumber("telephoneNumber");
+        companyInfoDO.setMail("mail");
+        companyInfoDO.setWebUrl("weburl");
+        companyInfoDO.setStory("story");
+        companyInfoDO.setVideoUrl("videoUrl");
+
+        companyInfoDAO.replaceCompanyInfoData(companyInfoDO);
     }
 }
