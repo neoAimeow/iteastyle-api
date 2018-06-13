68228722579bbe65f26bc630eb9130e68b5d608a
zhaoyi.w <a18814888787@gmail.com>
2018-06-13 23:51:40 +0800

    update these code

--- iteastyle-service/src/main/java/com/aimeow/iteastyle/manager/PostManager.java
+++ iteastyle-service/src/main/java/com/aimeow/iteastyle/manager/PostManager.java
@@ -1,4 +1,18 @@
-package com.aimeow.iteastyle.manager.impl;
+package com.aimeow.iteastyle.manager;
+
+import java.util.List;
+
+import com.aimeow.iteastyle.domain.PostBO;
 
 public interface PostManager {
+
+    List<PostBO> getPosts(
+        Integer status , Integer page , Integer pageSize
+    ) throws Exception;
+
+    PostBO getPostById(String postId) throws Exception;
+
+    Boolean createPost(PostBO postBO) throws Exception;
+    Boolean updatePost(PostBO postBO) throws Exception;
+    Boolean deletePost(String postId) throws Exception;
 }
--- iteastyle-service/src/main/java/com/aimeow/iteastyle/manager/impl/PostManagerImpl.java
+++ iteastyle-service/src/main/java/com/aimeow/iteastyle/manager/impl/PostManagerImpl.java
@@ -1,4 +1,36 @@
 package com.aimeow.iteastyle.manager.impl;
 
-public class PostManagerImpl {
+import java.util.List;
+
+import com.aimeow.iteastyle.domain.PostBO;
+import com.aimeow.iteastyle.manager.PostManager;
+import lombok.NonNull;
+
+public class PostManagerImpl implements PostManager {
+
+    @Override public List<PostBO> getPosts(
+        @NonNull Integer status, Integer page, Integer pageSize
+    ) throws Exception {
+        return null;
+    }
+
+    @Override public PostBO getPostById(
+        @NonNull String postId) throws Exception {
+        return null;
+    }
+
+    @Override public Boolean createPost(
+        @NonNull PostBO postBO) throws Exception {
+        return null;
+    }
+
+    @Override public Boolean updatePost(
+        @NonNull PostBO postBO) throws Exception {
+        return null;
+    }
+
+    @Override public Boolean deletePost(
+        @NonNull String postId) throws Exception {
+        return null;
+    }
 }
