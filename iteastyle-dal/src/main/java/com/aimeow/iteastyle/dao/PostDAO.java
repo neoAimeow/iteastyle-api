7ff46c8b2a63172605d17c70ba4ee4208ef1c093
zhaoyi.w <a18814888787@gmail.com>
2018-06-13 20:33:55 +0800

    update these code

--- iteastyle-dal/src/main/java/com/aimeow/iteastyle/dao/PostDAO.java
+++ iteastyle-dal/src/main/java/com/aimeow/iteastyle/dao/PostDAO.java
@@ -1,4 +1,13 @@
 package com.aimeow.iteastyle.dao;
 
+import java.util.List;
+
+import com.aimeow.iteastyle.domain.PostDO;
+
 public interface PostDAO {
+    List<PostDO> queryPosts() throws Exception;
+    PostDO queryPostById() throws Exception;
+    Boolean createPost(PostDO postDO) throws Exception;
+    Boolean updatePost(PostDO postDO) throws Exception;
+    Boolean deletePostById(String postId) throws Exception;
 }
---
+++ iteastyle-dal/src/main/java/com/aimeow/iteastyle/dao/impl/PostDAOImpl.java
@@ -0,0 +1,33 @@
+package com.aimeow.iteastyle.dao.impl;
+
+import java.util.List;
+
+import com.aimeow.iteastyle.dao.PostDAO;
+import com.aimeow.iteastyle.domain.PostDO;
+
+public class PostDAOImpl implements PostDAO {
+    @Override
+    public List<PostDO> queryPosts() throws Exception {
+        return null;
+    }
+
+    @Override
+    public PostDO queryPostById() throws Exception {
+        return null;
+    }
+
+    @Override
+    public Boolean createPost(PostDO postDO) throws Exception {
+        return null;
+    }
+
+    @Override
+    public Boolean updatePost(PostDO postDO) throws Exception {
+        return null;
+    }
+
+    @Override
+    public Boolean deletePostById(String postId) throws Exception {
+        return null;
+    }
+}
