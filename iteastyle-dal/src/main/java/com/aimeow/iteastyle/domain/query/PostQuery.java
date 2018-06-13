6db46dde988234b9e19ceb456291836d21ba10dd
zhaoyi.w <a18814888787@gmail.com>
2018-06-14 00:08:38 +0800

    update these code

--- iteastyle-dal/src/main/java/com/aimeow/iteastyle/domain/query/PostQuery.java
+++ iteastyle-dal/src/main/java/com/aimeow/iteastyle/domain/query/PostQuery.java
@@ -3,7 +3,7 @@ package com.aimeow.iteastyle.domain.query;
 import lombok.Data;
 
 @Data
-public class PostQuery {
+public class PostQuery extends BaseQuery {
     private String postId;
     private Integer status;
 }
--- iteastyle-service/src/main/java/com/aimeow/iteastyle/manager/impl/PostManagerImpl.java
+++ iteastyle-service/src/main/java/com/aimeow/iteastyle/manager/impl/PostManagerImpl.java
@@ -1,10 +1,14 @@
 package com.aimeow.iteastyle.manager.impl;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import com.aimeow.iteastyle.converter.PostConverter;
 import com.aimeow.iteastyle.dao.PostDAO;
 import com.aimeow.iteastyle.domain.PostBO;
+import com.aimeow.iteastyle.domain.PostDO;
 import com.aimeow.iteastyle.domain.Result;
+import com.aimeow.iteastyle.domain.query.PostQuery;
 import com.aimeow.iteastyle.manager.PostManager;
 import lombok.NonNull;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,34 +21,53 @@ public class PostManagerImpl implements PostManager {
         @NonNull Integer status, Integer page, Integer pageSize
     ) throws Exception {
         Result<List<PostBO>> result = new Result<>();
-
+        PostQuery query = new PostQuery();
+        query.setPage((page==null || page==0)?1:page);
+        query.setPageSize((pageSize==null || pageSize==0)?10:pageSize);
+        query.setStartRow((page-1) * pageSize);
+        query.setStatus(status);
+        List<PostBO> postBOS = new ArrayList<>();
+        List<PostDO> postDOS = postDAO.queryPosts(query);
+        postDOS.iterator().forEachRemaining(
+            obj->{
+                postBOS.add(PostConverter.convertDTB(obj));
+            }
+        );
+        result.setModel(postBOS);
         return result;
     }
 
     @Override public Result<PostBO> getPostById(
         @NonNull String postId) throws Exception {
         Result<PostBO> result = new Result<>();
-
+        PostQuery query = new PostQuery();
+        query.setPostId(postId);
+        result.setModel(PostConverter
+            .convertDTB(postDAO.queryPostById(query)));
         return result;
     }
 
     @Override public Result<Boolean> createPost(
         @NonNull PostBO postBO) throws Exception {
         Result<Boolean> result = new Result<>();
-        //postDAO.createPost(postBO)
+        result.setModel(postDAO.createPost(
+            PostConverter.convertBTD(postBO)));
         return result;
     }
 
     @Override public Result<Boolean> updatePost(
         @NonNull PostBO postBO) throws Exception {
         Result<Boolean> result = new Result<>();
+        result.setModel(postDAO.updatePost(
+            PostConverter.convertBTD(postBO)));
         return result;
     }
 
     @Override public Result<Boolean> deletePost(
         @NonNull String postId) throws Exception {
-
         Result<Boolean> result = new Result<>();
+        result.setModel(postDAO.deletePostById(
+            postId));
         return result;
     }
 }
