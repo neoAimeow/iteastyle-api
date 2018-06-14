a4d943f69fca317a24087098fc6611beb656c8d4
zhaoyi.w <a18814888787@gmail.com>
2018-06-15 03:36:07 +0800

    update these code

--- iteastyle-service/src/main/java/com/aimeow/iteastyle/manager/StaticDataManager.java
+++ iteastyle-service/src/main/java/com/aimeow/iteastyle/manager/StaticDataManager.java
@@ -1,7 +1,7 @@
 package com.aimeow.iteastyle.manager;
 
-import com.aimeow.iteastyle.domain.Result;
-import com.aimeow.iteastyle.domain.StaticDataBO;
+        import com.aimeow.iteastyle.domain.Result;
+        import com.aimeow.iteastyle.domain.StaticDataBO;
 
 public interface StaticDataManager {
     Result<StaticDataBO> getStaticData() throws Exception;
