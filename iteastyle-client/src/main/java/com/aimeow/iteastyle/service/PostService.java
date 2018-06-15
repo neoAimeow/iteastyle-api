fc53012030a1e5ee7462efce1019f3ece4dfa503
zhaoyi.w <a18814888787@gmail.com>
2018-06-15 15:59:16 +0800

    update these code

--- iteastyle-client/src/main/java/com/aimeow/iteastyle/service/PostService.java
+++ iteastyle-client/src/main/java/com/aimeow/iteastyle/service/PostService.java
@@ -5,7 +5,9 @@ import java.util.List;
 import com.aimeow.iteastyle.domain.GetPostsResult;
 import com.aimeow.iteastyle.domain.PostVO;
 import com.aimeow.iteastyle.domain.Result;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public interface PostService {
     GetPostsResult getPosts(
         Integer page, Integer pageSize
