60195fb19cdefb759e98ad425f223b02ef80ff02
zhaoyi.w <a18814888787@gmail.com>
2018-06-15 13:41:44 +0800

    update these code

--- iteastyle-war/src/main/java/com/aimeow/iteastyle/Application.java
+++ iteastyle-war/src/main/java/com/aimeow/iteastyle/Application.java
@@ -1,5 +1,8 @@
 package com.aimeow.iteastyle;
 
+import com.aimeow.iteastyle.domain.CompanyInfoVO;
+import com.aimeow.iteastyle.service.CompanyInfoService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.*;
 import org.springframework.boot.autoconfigure.*;
 import org.springframework.context.annotation.ComponentScan;
@@ -8,10 +11,12 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @SpringBootApplication
 public class Application {
+    @Autowired
+    private CompanyInfoService companyInfoService;
 
     @RequestMapping("/")
-    String home() {
-        return "Hello world";
+    CompanyInfoVO home() {
+        return companyInfoService.getCompanyInfo().getModel();
         //TestObject testObject = new TestObject();
         //return testObject.hello();
     }
