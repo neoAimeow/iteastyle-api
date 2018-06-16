969365e61ffaed04a003d0f1bfb88822c195a821
zhaoyi.w <a18814888787@gmail.com>
2018-06-16 19:08:41 +0800

    update these code

--- iteastyle-service/src/main/java/com/aimeow/iteastyle/service/impl/AdminServiceImpl.java
+++ iteastyle-service/src/main/java/com/aimeow/iteastyle/service/impl/AdminServiceImpl.java
@@ -1,15 +1,42 @@
 package com.aimeow.iteastyle.service.impl;
 
+import com.aimeow.iteastyle.domain.Result;
 import com.aimeow.iteastyle.manager.CompanyInfoManager;
 import com.aimeow.iteastyle.manager.PostManager;
 import com.aimeow.iteastyle.manager.StaticDataManager;
+import com.aimeow.iteastyle.service.AdminService;
+import com.alibaba.fastjson.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
-public class AdminServiceImpl {
+public class AdminServiceImpl implements AdminService {
     @Autowired private CompanyInfoManager companyInfoManager;
     @Autowired private StaticDataManager staticDataManager;
     @Autowired private PostManager postManager;
 
+    @Override
+    public Result<Boolean> updateCompanyInfo(JSONObject param) {
+        return null;
+    }
+
+    @Override
+    public Result<Boolean> updateStaticData(JSONObject param) {
+        return null;
+    }
+
+    @Override
+    public Result<Boolean> createPost(JSONObject param) {
+        return null;
+    }
+
+    @Override
+    public Result<Boolean> updatePost(JSONObject param) {
+        return null;
+    }
+
+    @Override
+    public Result<Boolean> deletePost(String postId) {
+        return null;
+    }
 }
