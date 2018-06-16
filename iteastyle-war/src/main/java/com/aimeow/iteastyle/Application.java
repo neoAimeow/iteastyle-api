fed1f35956895cf519c60e2f58684bfae7a90f59
zhaoyi.w <a18814888787@gmail.com>
2018-06-16 19:37:13 +0800

    update these code

--- iteastyle-war/src/main/java/com/aimeow/iteastyle/Application.java
+++ iteastyle-war/src/main/java/com/aimeow/iteastyle/Application.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootConfiguration
 @EnableAutoConfiguration
 @RestController
-@ComponentScan({"com.aimeow.iteastyle.service","com.aimeow.iteastyle.manager"})
+@ComponentScan({"com.aimeow.iteastyle.service","com.aimeow.iteastyle.manager","com.aimeow.iteastyle.dao"})
 public class Application {
     @RequestMapping("/")
     public String greet() {
