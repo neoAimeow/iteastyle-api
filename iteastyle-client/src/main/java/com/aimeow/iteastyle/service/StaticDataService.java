cdc2ab974f3234e32cd3affe787572cbf7c13aa3
zhaoyi.w <a18814888787@gmail.com>
2018-06-16 15:12:42 +0800

    update these code

--- iteastyle-client/src/main/java/com/aimeow/iteastyle/service/StaticDataService.java
+++ iteastyle-client/src/main/java/com/aimeow/iteastyle/service/StaticDataService.java
@@ -2,9 +2,11 @@ package com.aimeow.iteastyle.service;
 
 import com.aimeow.iteastyle.domain.Result;
 import com.aimeow.iteastyle.domain.StaticDataVO;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public interface StaticDataService {
+    @RequestMapping("/staticData/getData")
     Result<StaticDataVO> getStaticData();
 }
