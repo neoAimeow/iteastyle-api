7e806fb1ab5d053a883fd1a7a39e47f94d902601
zhaoyi.w <a18814888787@gmail.com>
2018-06-16 19:18:27 +0800

    update these code

--- iteastyle-client/src/main/java/com/aimeow/iteastyle/service/AdminService.java
+++ iteastyle-client/src/main/java/com/aimeow/iteastyle/service/AdminService.java
@@ -2,13 +2,23 @@ package com.aimeow.iteastyle.service;
 
 import com.aimeow.iteastyle.domain.CompanyInfoVO;
 import com.aimeow.iteastyle.domain.Result;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.alibaba.fastjson.JSONObject;
 @RestController
 public interface AdminService {
+    @RequestMapping("/admin/updateCompanyInfo")
     Result<Boolean> updateCompanyInfo(JSONObject param);
+
+    @RequestMapping("/admin/updateStaticData")
     Result<Boolean> updateStaticData(JSONObject param);
+
+    @RequestMapping("/admin/createPost")
     Result<Boolean> createPost(JSONObject param);
+
+    @RequestMapping("/admin/updatePost")
     Result<Boolean> updatePost(JSONObject param);
+
+    @RequestMapping("/admin/deletePost")
     Result<Boolean> deletePost(String postId);
 }
