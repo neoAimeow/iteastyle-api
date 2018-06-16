4388df711c409523a3f7526b11cf677f627bd709
zhaoyi.w <a18814888787@gmail.com>
2018-06-16 22:22:42 +0800

    update these code

--- iteastyle-war/src/main/java/com/aimeow/iteastyle/Application.java
+++ iteastyle-war/src/main/java/com/aimeow/iteastyle/Application.java
@@ -7,10 +7,12 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootConfiguration
 @EnableAutoConfiguration
 @RestController
+@EnableSwagger2
 @ComponentScan({"com.aimeow.iteastyle.service"
         ,"com.aimeow.iteastyle.manager"
         ,"com.aimeow.iteastyle.dao"})
