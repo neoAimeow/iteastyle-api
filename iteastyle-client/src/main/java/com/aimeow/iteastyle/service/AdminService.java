745f1f17e0734ae7e006ce287dcf2bd4660d0bc4
zhaoyi.w <a18814888787@gmail.com>
2018-06-17 00:22:30 +0800

    update these code

--- iteastyle-client/src/main/java/com/aimeow/iteastyle/service/AdminService.java
+++ iteastyle-client/src/main/java/com/aimeow/iteastyle/service/AdminService.java
@@ -9,21 +9,22 @@ import org.springframework.web.bind.annotation.RestController;
 import com.alibaba.fastjson.JSONObject;
 
 @Api(value = "AdminService", description = "管理相关各路接口")
+@RequestMapping(value = "/admin")
 @RestController
 public interface AdminService {
 
-    @RequestMapping(value = "/admin/updateCompanyInfo", method = RequestMethod.POST, consumes="application/json")
+    @RequestMapping(value = "/updateCompanyInfo", method = RequestMethod.POST, consumes="application/json")
     Result<Boolean> updateCompanyInfo(JSONObject param);
 
-    @RequestMapping(value = "/admin/updateStaticData", method = RequestMethod.POST, consumes="application/json")
+    @RequestMapping(value = "/updateStaticData", method = RequestMethod.POST, consumes="application/json")
     Result<Boolean> updateStaticData(JSONObject param);
 
-    @RequestMapping(value = "/admin/createPost", method = RequestMethod.POST, consumes="application/json")
+    @RequestMapping(value = "/createPost", method = RequestMethod.POST, consumes="application/json")
     Result<Boolean> createPost(JSONObject param);
 
-    @RequestMapping(value = "/admin/updatePost", method = RequestMethod.POST, consumes="application/json")
+    @RequestMapping(value = "/updatePost", method = RequestMethod.POST, consumes="application/json")
     Result<Boolean> updatePost(JSONObject param);
 
-    @RequestMapping(value = "/admin/deletePost", method = RequestMethod.POST, consumes="application/json")
+    @RequestMapping(value = "/deletePost", method = RequestMethod.POST, consumes="application/json")
     Result<Boolean> deletePost(String postId);
 }
