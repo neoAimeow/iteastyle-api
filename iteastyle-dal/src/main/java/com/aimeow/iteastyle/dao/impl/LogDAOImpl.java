e3d3d930a4434f53dd9e20371f386351179c079d
zhaoyi.w <a18814888787@gmail.com>
2018-06-17 13:18:13 +0800

    update these code

--- iteastyle-dal/src/main/java/com/aimeow/iteastyle/dao/impl/LogDAOImpl.java
+++ iteastyle-dal/src/main/java/com/aimeow/iteastyle/dao/impl/LogDAOImpl.java
@@ -3,21 +3,37 @@ package com.aimeow.iteastyle.dao.impl;
 import com.aimeow.iteastyle.dao.LogDAO;
 import com.aimeow.iteastyle.domain.LogDO;
 import com.aimeow.iteastyle.domain.query.LogQuery;
+import lombok.NonNull;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+
+import java.util.Date;
 
 public class LogDAOImpl implements LogDAO {
+    @Autowired private MongoTemplate mongoTemplate;
     @Override
-    public LogDO queryLogs(LogQuery query) {
+    public LogDO queryLogs(@NonNull LogQuery query) {
+
         return null;
     }
 
     @Override
-    public Boolean record(LogDO logDO) {
-        return null;
+    public Boolean record(
+            @NonNull LogDO logDO) {
+        logDO.setGmtCreate(new Date());
+        logDO.setGmtModified(new Date());
+        mongoTemplate.save(logDO);
+        return true;
     }
 
     @Override
-    public Boolean removeRecord(String logId) {
-        return null;
+    public Boolean removeRecord(@NonNull String logId) {
+        Query query=new Query(
+                Criteria.where("id").is(logId));
+        mongoTemplate.remove(query,LogDO.class);
+        return true;
     }
 
 
--- iteastyle-dal/src/main/java/com/aimeow/iteastyle/domain/LogDO.java
+++ iteastyle-dal/src/main/java/com/aimeow/iteastyle/domain/LogDO.java
@@ -1,4 +1,16 @@
 package com.aimeow.iteastyle.domain;
 
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+
+import java.util.Date;
+
+@Data
 public class LogDO {
+    @Id
+    private String id;
+    private String content;
+    private String operator;
+    private Date gmtCreate;
+    private Date gmtModified;
 }
