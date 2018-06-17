76efc8aabb6df226711c6fec44cae7f9dace2788
zhaoyi.w <a18814888787@gmail.com>
2018-06-17 16:56:36 +0800

    update these code

--- iteastyle-client/src/main/java/com/aimeow/iteastyle/service/CaseService.java
+++ iteastyle-client/src/main/java/com/aimeow/iteastyle/service/CaseService.java
@@ -3,8 +3,22 @@ package com.aimeow.iteastyle.service;
 import com.aimeow.iteastyle.domain.CaseVO;
 import com.aimeow.iteastyle.domain.GetCasesVO;
 import com.aimeow.iteastyle.domain.Result;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
+@Api(value = "/case", tags = {"经典案例"},description = "经典案例相关接口")
+@RequestMapping(value = "/case", consumes="application/json")
+@RestController
 public interface CaseService {
+
+    @ApiOperation(value = "通过经典案例ID获取经典案例内容")
+    @RequestMapping(value = "/getCaseById", method = RequestMethod.GET)
     Result<CaseVO> getCaseById(String caseId);
+
+    @ApiOperation(value = "通过分页参数获得经典案例列表")
+    @RequestMapping(value = "/cases", method = RequestMethod.GET)
     Result<GetCasesVO> getCases(Integer page , Integer pageSize);
 }
--- iteastyle-client/src/main/java/com/aimeow/iteastyle/service/ProductShowerService.java
+++ iteastyle-client/src/main/java/com/aimeow/iteastyle/service/ProductShowerService.java
@@ -3,10 +3,23 @@ package com.aimeow.iteastyle.service;
 import com.aimeow.iteastyle.domain.GetProductShowersVO;
 import com.aimeow.iteastyle.domain.ProductShowerVO;
 import com.aimeow.iteastyle.domain.Result;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
+@Api(value = "/productShower", tags = {"产品展示"},description = "产品展示相关接口")
+@RequestMapping(value = "/productShower", consumes="application/json")
+@RestController
 public interface ProductShowerService {
 
+    @ApiOperation(value = "通过产品展示ID获取经典案例内容")
+    @RequestMapping(value = "/productShowerById", method = RequestMethod.GET)
     Result<ProductShowerVO> getProductShowerById(String productShowerId);
-    Result<GetProductShowersVO> getProducts(Integer page , Integer pageSize);
+
+    @ApiOperation(value = "通过分页参数获得产品展示列表")
+    @RequestMapping(value = "/productShowers", method = RequestMethod.GET)
+    Result<GetProductShowersVO> getProductShowers(Integer page , Integer pageSize);
 
 }
