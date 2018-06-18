e60cefa7c3eaa41a5462106487a4578d947af238
zhaoyi.w <a18814888787@gmail.com>
2018-06-18 21:05:24 +0800

    update these code

--- iteastyle-service/src/main/java/com/aimeow/iteastyle/manager/LogManager.java
+++ iteastyle-service/src/main/java/com/aimeow/iteastyle/manager/LogManager.java
@@ -6,8 +6,9 @@ import com.aimeow.iteastyle.domain.Result;
 import java.util.List;
 
 public interface LogManager {
-    Result<List<LogBO>> queryLogs() throws Exception;
-    Result<Boolean> record() throws Exception;
+    Result<List<LogBO>> queryLogs(
+            Integer page , Integer pageSize) throws Exception;
+    Result<Boolean> record(LogBO logBO) throws Exception;
     Result<Boolean> removeLogById(String logId) throws Exception;
     Result<Boolean> clearLogs() throws Exception;
 }
--- iteastyle-service/src/main/java/com/aimeow/iteastyle/manager/impl/LogManagerImpl.java
+++ iteastyle-service/src/main/java/com/aimeow/iteastyle/manager/impl/LogManagerImpl.java
@@ -1,34 +1,56 @@
 package com.aimeow.iteastyle.manager.impl;
 
+import com.aimeow.iteastyle.converter.LogConverter;
 import com.aimeow.iteastyle.dao.LogDAO;
 import com.aimeow.iteastyle.domain.LogBO;
+import com.aimeow.iteastyle.domain.LogDO;
 import com.aimeow.iteastyle.domain.Result;
+import com.aimeow.iteastyle.domain.query.LogQuery;
 import com.aimeow.iteastyle.manager.LogManager;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class LogManagerImpl implements LogManager {
     @Autowired private LogDAO logDAO;
 
     @Override
-    public Result<List<LogBO>> queryLogs() {
-
-        return null;
+    public Result<List<LogBO>> queryLogs(
+            Integer page , Integer pageSize) throws Exception {
+        Result<List<LogBO>> result = new Result<>();
+        List<LogBO> logBOS = new ArrayList<>();
+        LogQuery query = new LogQuery();
+        query.setPage(page);
+        query.setPageSize(pageSize);
+        List<LogDO> logDOS = logDAO.queryLogs(query);
+        logDOS.iterator().forEachRemaining(
+                obj-> {
+                    logBOS.add(LogConverter.convertDTB(obj));
+                }
+        );
+        result.setModel(logBOS);
+        return result;
     }
 
     @Override
-    public Result<Boolean> record() {
-        return null;
+    public Result<Boolean> record(LogBO logBO) throws Exception {
+        Result<Boolean> result = new Result<>();
+        result.setModel(logDAO.record(LogConverter.convertBTD(logBO)));
+        return result;
     }
 
     @Override
-    public Result<Boolean> clearLogs() {
-        return null;
+    public Result<Boolean> clearLogs() throws Exception {
+        Result<Boolean> result = new Result<>();
+        result.setModel(logDAO.clearRecord());
+        return result;
     }
 
     @Override
-    public Result<Boolean> removeLogById(String logId) {
-        return null;
-    }
+    public Result<Boolean> removeLogById(String logId) throws Exception {
+        Result<Boolean> result = new Result<>();
+        result.setModel(logDAO.removeRecord(logId));
+        return result;
+    }Z
 }
