bf11431588a5068c5efbfd6b0807ffd196d58aaf
zhaoyi.w <a18814888787@gmail.com>
2018-06-24 18:54:58 +0800

    update these code

--- iteastyle-war/src/main/java/com/aimeow/iteastyle/config/WebConfig.java
+++ iteastyle-war/src/main/java/com/aimeow/iteastyle/config/WebConfig.java
@@ -1,4 +1,4 @@
-package com.aimeow.iteastyle;
+package com.aimeow.iteastyle.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
