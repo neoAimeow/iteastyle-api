b4ed0862711bc54c0ed2eaccdffb8de043dc44db
zhaoyi.w <a18814888787@gmail.com>
2018-06-26 20:47:46 +0800

    update these code

--- iteastyle-service/src/main/java/com/aimeow/iteastyle/service/impl/QiniuServiceImpl.java
+++ iteastyle-service/src/main/java/com/aimeow/iteastyle/service/impl/QiniuServiceImpl.java
@@ -3,11 +3,13 @@ package com.aimeow.iteastyle.service.impl;
 import com.aimeow.iteastyle.domain.Result;
 import com.aimeow.iteastyle.service.QiniuService;
 import com.qiniu.util.Auth;
+import org.springframework.stereotype.Component;
 
+@Component
 public class QiniuServiceImpl implements QiniuService {
-    private static String accessKey = "你的AK";
-    private static String secretKey = "你的SK";
-    private static String bucket = "你的存储空间名";
+    private static String accessKey = "7hKfN1Ymb8704XrZr09Hh1FpVLAHBzDFn7hxel30";
+    private static String secretKey = "0w9SrQKJgZZk5zAjAKC1Ec3v2XCEtgv-FBmAPILc";
+    private static String bucket = "iteastyle";
 
     @Override
     public Result<String> getUploadToken() {
