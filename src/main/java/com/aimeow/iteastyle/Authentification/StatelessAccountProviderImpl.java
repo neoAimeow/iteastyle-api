d1edae5c6c0de8872c325f27f4ba5b6513f2a3b0
映溪 <zhaoyi.w@alibaba-inc.com>
2018-07-25 15:43:00 +0800

    update code

---
+++ src/main/java/com/aimeow/iteastyle/Authentification/StatelessAccountProviderImpl.java
@@ -0,0 +1,9 @@
+package com.aimeow.iteastyle.Authentification;
+
+/**
+ * Created on 2018/7/25 3:34 PM
+ *
+ * @author zhaoyi.w <zhaoyi.w@alibaba-inc.com>
+ */
+public class StatelessAccountProviderImpl implements ShiroStatelessAccountProvider {
+}
