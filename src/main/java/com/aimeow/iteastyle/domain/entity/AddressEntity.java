65a583b31ca1320d229c12c4590fb374a417478f
zhaoyi.w <a18814888787@gmail.com>
2018-11-06 10:29:54 +0800

    update these code

--- src/main/java/com/aimeow/iteastyle/domain/entity/AddressEntity.java
+++ src/main/java/com/aimeow/iteastyle/domain/entity/AddressEntity.java
@@ -7,12 +7,20 @@ import lombok.EqualsAndHashCode;
 @Data
 @EqualsAndHashCode(callSuper=true)
 public class AddressEntity extends BaseEntity {
+    /** 对应的userId */
+    private String userId;
     /** 联系人（收件人） */
     private String consignee;
     /** 性别 */
     private Integer gender;
     /** 手机号 */
     private String phoneNo;
+    /** 省 */
+    private String province;
+    /** 市 */
+    private String city;
+    /** 区 */
+    private String district;
     /** 地址 */
     private String address;
     /** 地址详情 */
@@ -21,4 +29,8 @@ public class AddressEntity extends BaseEntity {
     private String tag;
     /** 是否默认 */
     private Boolean isDefault;
+    /** 纬度 */
+    private Double latitude;
+    /** 经度 */
+    private Double longitude;
 }
